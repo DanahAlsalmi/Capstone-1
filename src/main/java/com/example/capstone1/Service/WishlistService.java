de2831c1ee3d0070fca0e47324f690fcf1debd96
Danah alsalmi <118121745+DanahAlsalmi@users.noreply.github.com>
2024-08-05 19:27:33 +0300

    endpoints22

---
+++ src/main/java/com/example/capstone1/Service/WishlistService.java
@@ -0,0 +1,50 @@
+package com.example.capstone1.Service;
+
+
+import com.example.capstone1.Model.Product;
+import com.example.capstone1.Model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+@RequiredArgsConstructor
+public class WishlistService {
+
+    private final UserService userService;
+    private final ProductService productService;
+
+
+    public void addItemToWishlist(int userId, int productId) {
+
+        User user = userService.getUserById(userId);
+        if (user != null) {
+            Product product = productService.getProductById(productId);
+            if (product != null) {
+                user.getWishlist().add(product);
+            }
+        }
+    }
+
+    public ArrayList<Product> getUserWishlist(int userId) {
+        User user = userService.getUserById(userId);
+        if (user != null) {
+            return user.getWishlist();
+        }
+        return null;
+    }
+
+    public void removeItemFromWishlist(int userId, int productId) {
+        User user = userService.getUserById(userId);
+        if (user != null) {
+            ArrayList<Product> wishlist = user.getWishlist();
+            for (int i = 0; i < wishlist.size(); i++) {
+                if (wishlist.get(i).getId() == productId) {
+                    wishlist.remove(i);
+                    break;
+                }
+            }
+        }
+    }
+}
